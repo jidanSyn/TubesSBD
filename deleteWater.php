64d72367c484e6bc3b7674e544b124c3fa82f331
WildanRaffians <134181656+WildanRaffians@users.noreply.github.com>
2023-06-01 17:10:33 +0700

    fitur delete sumber air

--- deleteWater.php
+++ deleteWater.php
@@ -1,1 +1,25 @@
-test delete
+<?php
+    include('function.php');
+
+
+    $id = $_GET['id'];
+    if ($id > 0) {
+        
+        $isDeleteSucceed = deleteWater($id); 
+        if ($isDeleteSucceed > 0) {
+        echo "
+        <script>
+        alert('Delete Success !');
+        document.location.href = 'admin.php';
+        </script>
+        ";
+        } else {
+        echo "
+        <script>
+        alert('Delete Failed !');
+        document.location.href = 'admin.php';
+        </script>
+        ";
+    }
+    }
+?>
